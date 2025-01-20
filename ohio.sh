748b12a723dedcdd6c22a40f24b3c0fca3fa2fe1
thines8899 <90979274+thines8899@users.noreply.github.com>
2025-01-20 20:07:12 +0800

    Update ohio.sh

--- ohio.sh
+++ ohio.sh
@@ -83,7 +83,7 @@ for region in "${!region_image_map[@]}"; do
         --version-description "Version1" \
         --launch-template-data "{
             \"ImageId\": \"$image_id\",
-            \"InstanceType\": \"c7i.16xlarge\",
+            \"InstanceType\": \"c7a.16xlarge\",
             \"KeyName\": \"$key_name\",
             \"SecurityGroupIds\": [\"$sg_id\"],
             \"UserData\": \"$user_data_base64\"
@@ -119,7 +119,7 @@ for region in "${!region_image_map[@]}"; do
     instance_id=$(aws ec2 run-instances \
         --image-id "$image_id" \
         --count 1 \
-        --instance-type c7i.16xlarge \
+        --instance-type c7a.16xlarge \
         --key-name "$key_name" \
         --security-group-ids "$sg_id" \
         --user-data "$user_data_base64" \
