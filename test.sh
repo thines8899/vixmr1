b0060a6714d63f9cf40c79e4a75b056f84376762
thines8899 <90979274+thines8899@users.noreply.github.com>
2025-01-27 01:56:28 +0800

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Định nghĩa Launch Template cho từng vùng
-declare -A REGION_TEMPLATES
+declare -A REGION_TEMPLATES (
 REGION_TEMPLATES["us-east-1"]="SpotLaunchTemplate-us-east-1"
 REGION_TEMPLATES["us-west-2"]="SpotLaunchTemplate-us-west-2"
 REGION_TEMPLATES["us-east-2"]="SpotLaunchTemplate-us-east-2"
-
+)
 # Số lượng instances cần tạo ở mỗi vùng
 INSTANCE_COUNT=8
 
